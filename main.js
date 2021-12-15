f6532d66a59676125127a1de066dc2ecefa8917e
Stanislav Sikorskyi <stanislavsikorskyi@MacBook-Pro-Stanislav.local>
2021-12-15 23:28:03 +0200

    Added select of To token

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-// ES6 Minimized
-
-
-
 /** Connect to Moralis server */
 const serverUrl = "https://y1md02ku7dmh.usemoralis.com:2053/server";
 const appId = "3genlm4qoZgQleuTwsX4PVheWSnSNx1fgirpsgki";
 Moralis.start({ serverUrl, appId });
 
-
 let currentTrade = {};
 let currentSelectedFrom;
+let currentSelectedTo;
 let tokensMap =  new Map();
 let tokensList;
+let modalType = 0;
 
 
 /** Add from here down */
@@ -60,35 +57,61 @@ async function listTokens() {
 
 function selectDefaultToken() {
   currentSelectedFrom = tokensMap.get("ETH");
-  console.log("Default selected " + currentSelectedFrom);
-  renderSelected();
+  console.log("Default selected from " + currentSelectedFrom);
+  renderSelectedFrom(currentSelectedFrom);
+
+  currentSelectedTo = tokensMap.get("USDT");
+  console.log("Default selected to " + currentSelectedFrom);
+  renderSelectedTo();
 }
 
 function selectToken() {
+  
+  console.log("selectToken");
+  if(modalType == 1) {
+      currentSelectedFrom = JSON.parse(event.target.getAttribute("selected_token"));
+      console.log(currentSelectedFrom);
+      renderSelectedFrom();
+  } else if (modalType == 2) {
+      currentSelectedTo = JSON.parse(event.target.getAttribute("selected_token"));
+      console.log(currentSelectedTo);
+      renderSelectedTo();
+  }
   closeModal();
-  currentSelectedFrom = JSON.parse(event.target.getAttribute("selected_token"));
-  console.log(currentSelectedFrom);
-  renderSelected(currentSelectedFrom.symbol);
 }
 
-function renderSelected() {
+function renderSelectedFrom() {
   document.getElementById("from_image").src = currentSelectedFrom.logoURI;
   document.getElementById("from_token").innerHTML  = currentSelectedFrom.symbol;
 }
 
-function openModal() {
+function renderSelectedTo() {
+  document.getElementById("to_image").src = currentSelectedTo.logoURI;
+  document.getElementById("to_token").innerHTML  = currentSelectedTo.symbol;
+}
+
+function openModal(type) {
   document.getElementById("token_modal").style.display = "block";
+  modalType = type;
 }
 
 function closeModal() {
   document.getElementById("token_modal").style.display = "none";
+  modalType = 0;
 }
-
 
 init();
 
 document.getElementById("modal_close").onclick = closeModal;
-document.getElementById("from_currency_container").onclick = openModal;
+// document.getElementById("from_currency_container").onclick = openModal;
+// document.getElementById("to_currency_container").onclick = openModal;
+
+document.getElementById("from_currency_container").addEventListener("click", function() {
+    openModal(1);
+}, false);
+document.getElementById("to_currency_container").addEventListener("click", function() {
+    openModal(2);
+}, false);
 
 //document.getElementById(" ").onclick = login;
 
